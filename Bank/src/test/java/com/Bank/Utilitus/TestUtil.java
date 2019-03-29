a31c702bfce7497dc2e1cd20bb7eb9e08e0b1322
Dodosar <depechemode1989@mail.ru>
2019-03-29 14:50:29 +0200

    Added rough TestTimeStamp. And in ScreenShots method add Date() and convert to string and replace ":" to "_"

--- Bank/src/test/java/com/Bank/Utilitus/TestUtil.java
+++ Bank/src/test/java/com/Bank/Utilitus/TestUtil.java
@@ -1,6 +1,7 @@
 package com.Bank.Utilitus;
 
 import java.io.File;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -12,20 +13,20 @@ import com.Bank.base.TestBase;
 public class TestUtil extends TestBase{
 
 	public static String path;
+	public static File file;
 	
 	public static void captureScreenshot(ITestResult result) {
 		String screenshotName = result.getMethod().getMethodName().toString();
 		try {
-			
+			Date d = new Date(); 
 			TakesScreenshot ts = (TakesScreenshot) driver;
 			File source = ts.getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(source, new File(System.getProperty("user.dir") + "\\src\\test\\resources\\screensShots" + "\\" + screenshotName	+ ".png"));
+			file = new File(System.getProperty("user.dir") + "\\src\\test\\resources\\screensShots" + "\\" + screenshotName	+ d.toString().replace(":", "_").replace(" ", "_") + ".png");
+			FileUtils.copyFile(source,file);
 			String ESCAPE_PROPERTY = "org.uncommons.reportng.escape-output";
 			System.setProperty(ESCAPE_PROPERTY, "false");
-			path = System.getProperty("user.dir") + "\\src\\test\\resources\\screensShots" + "\\" + screenshotName
-					 + ".png";				
+			path = file.getAbsolutePath();
 		}
-
 		catch (Exception e) {
 			System.out.println("Exception while taking screenshot "
 					+ e.getMessage());
